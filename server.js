615d55ea410e1ba06c7e7e42aec38efc62af7435
Dikz <cicerogabriel12@hotmail.com>
2019-10-05 01:47:12 -0300

    Reposicionamento de req.io

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ class App {
 		this.http = require('http').Server(this.express)
 		this.io = require('socket.io')(this.http)
 
-		this.express.use(cors())
-		this.express.use(express.json())
-		this.express.use(require('./src/routes'))
-
-		this.io.on('connection', socket => {
-			console.log(`${socket.id} conectado!`)
-		})
-
 		this.express.use((req, res, next) => {
 			req.io = this.io
 
 			return next()
 		})
+
+		this.io.on('connection', socket => {
+			console.log(`${socket.id} conectado!`)
+		})
+
+		this.express.use(cors())
+		this.express.use(express.json())
+		this.express.use(require('./src/routes'))
 	}
 
 }
