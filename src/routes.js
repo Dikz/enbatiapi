5563bfb165506886ac0af74ec0fcd69840b1fdd4
Dikz <cicerogabriel12@hotmail.com>
2019-10-07 10:27:14 -0300

    Static com resolve

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-const fs = require('fs')
+const { resolve } = require('path')
 
 const Participant = require("./controllers/Participant")
 const Question = require("./controllers/Question")
@@ -9,7 +9,7 @@ routes.get('/', (req, res) => {
 	res.send({ message: "Hello World :)" })
 })
 
-routes.use('/images', express.static(__dirname + '/img'))
+routes.use('/images', express.static(resolve(__dirname, 'img')))
 
 routes.get('/participants', Participant.index)
 routes.post('/participants', Participant.store)
